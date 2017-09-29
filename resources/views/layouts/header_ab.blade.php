9fe35ccbe268fdd913f5319a0dd4317ff86d34a2
zhuangalbert <albertzuang@gmail.com>
2017-09-29 18:37:14 +0700

    First_commit

---
+++ resources/views/layouts/header_ab.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('bootstrap-3.3.7-dist/css/bootstrap.min.css') }}" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page
+    via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/
+html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/
+respond.min.js"></script>
+    <![endif]-->
+    <title>Bootstrap Template</title>
+</head>
+<body>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="{{ asset('bootstrap-3.3.7-dist/js/bootstrap.min.js') }}"></script>
+</body>
+</html>
---
+++ resources/views/layouts/master_ab.blade.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @include('layouts.header_ab')
+    <link href="{{ asset('css/master_ab.css') }}" rel="stylesheet">
+</head>
+<body>
+    {{--START HEADER--}}
+    <div class="header">
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">NA-TASK</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Insert Task</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    {{--END HEADER--}}
+    <div class="content">
+        <div class="container">
+            <div class="col-md-offset-4 col-md-4">
+                <div class="box">
+                    <div class="form-group">
+                        <div class="form-text">
+                            <span>Task Name</span>
+                        </div>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <div class="form-text">
+                            <span>Description</span>
+                        </div>
+                        <textarea class="form-control" rows="5"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <div class="form-text">
+                            <span>PIC</span>
+                        </div>
+                        <select class="form-control">
+                            <option>AB</option>
+                            <option>CA</option>
+                            <option>BP</option>
+                        </select>
+                    </div>
+                    <div class="text-center form-group btn-form-group">
+                        <button class="btn btn-default btn-submit">Insert</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
